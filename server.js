30432eacaab8e1c8dc34708b3dd7509e9dd08a53
peterslight <uremgbachisom@gmail.com>
2020-09-18 02:02:05 +0100

    setup postgres setup .env file setup routes insert files using postgresql setup dialect and intellij database tools

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const mustacheExpress = require('mustache-express')
 require('dotenv').config()
+const bodyParser = require('body-parser')
 const app = express()
+const {Client} = require('pg')
+
+module.exports = {
+    development: {
+        username: "root",
+        password: "ppeterstev",
+        database: "booklist",
+        host: "127.0.0.1",
+        dialect: 'postgres'
+    }
+}
 
 const mustache = mustacheExpress()
 mustache.cache = null;
 app.engine('mustache', mustache)
 app.set('view engine', 'mustache')
+
+//static file handling
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 // /list here is just a route
 app.get('/list', (req,
@@ -16,12 +35,28 @@ app.get('/list', (req,
     }
 )
 
-app.get('/book-form', (req, res) => {
+app.get('/book/add', (req, res) => {
     res.render('book-form')
 })
 
-//static file handling
-app.use(express.static('public'))
+app.post('/book/add', (req, res) => {
+    console.log('body: ', req.body)
+    //connect to db, run query and redirect to list page
+    const client = new Client()
+    client.connect().then(() => {
+        const sql = 'INSERT INTO books(title, authors) VALUES ($1, $2)'
+        const params = [req.body.title, req.body.authors]
+        return client.query(sql, params)
+    }).then((result) => {
+        console.log('result', result)
+        res.redirect('/list')
+    }).catch((error) => {
+        console.log(error.message)
+        res.redirect('/list')
+    })
+
+
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}`)
